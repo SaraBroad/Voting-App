ed813f44e3ea508e71f2a614c5721257433aaba5
Sara Broad <saraebroad@gmail.com>
2018-05-21 21:17:27 -0400

    dumby data

--- public/assets/js/vol-events.js
+++ public/assets/js/vol-events.js
@@ -3,20 +3,43 @@ $(document).ready(function () {
     function getEvents() {
         $.get("/api/events", function (data) {
             console.log(data);
-            if (data.length !== 0) {
+            var fakeData = [{
+                event_title: 'Coding Class',
+                event_description: "ksfsjfsd",
+                organization_name: "sdfksadkfdsk",
+                organizer_first_name: "dskdkdsksd",
+                organizer_last_name: "dsksdksdkdsksd",
+                street_address: "kdsksdksdks",
+                city: "Philadelphia",
+                state: "PA",
+                zip_code: 19148,
+                phone_number: 2155555555
+            }]
+            if (data.length === 0) {
                 for (var i = 0; i < data.length; i++) {
                     var row = $("<div>");
-                    row.append("<p>Event name: " + data[i].event_title + "<p>");
-                    row.append("<p>Event name: " + data[i].event_description + "<p>");
-                    row.append("<p>Event name: " + data[i].organization_name + "<p>");
-                    row.append("<p>Event name: " + data[i].organizer_first_name + "<p>");
-                    row.append("<p>Event name: " + data[i].organizer_last_name + "<p>");
-                    row.append("<p>Event name: " + data[i].street_address + "<p>");
-                    row.append("<p>Event name: " + data[i].city + ", " + data[i].state + " " + data[i].zip_code + "<p>");
-                    row.append("<p>Event name: " + data[i].state + "<p>");
-                    row.append("<p>Event name: " + data[i].phone_number + "<p>");
+                    row.append("<p>Event name: " + fakeData.event_title + "<p>");
+                    row.append("<p>Event name: " + fakeData.event_description + "<p>");
+                    row.append("<p>Event name: " + fakeData.organization_name + "<p>");
+                    row.append("<p>Event name: " + fakeData.organizer_first_name + "<p>");
+                    row.append("<p>Event name: " + fakeData.organizer_last_name + "<p>");
+                    row.append("<p>Event name: " + fakeData.street_address + "<p>");
+                    row.append("<p>Event name: " + fakeData.city + ", " + data[i].state + " " + data[i].zip_code + "<p>");
+                    row.append("<p>Event name: " + fakeData.state + "<p>");
+                    row.append("<p>Event name: " + fakeData.phone_number + "<p>");
                     // row.append("<p>Event name: " + data[i].time_slots + "<p>");
-                    $("#event-box").prepend(row);
+                    $(".event-holder").prepend(row);
+                    // row.append("<p>Event name: " + data[i].event_title + "<p>");
+                    // row.append("<p>Event name: " + data[i].event_description + "<p>");
+                    // row.append("<p>Event name: " + data[i].organization_name + "<p>");
+                    // row.append("<p>Event name: " + data[i].organizer_first_name + "<p>");
+                    // row.append("<p>Event name: " + data[i].organizer_last_name + "<p>");
+                    // row.append("<p>Event name: " + data[i].street_address + "<p>");
+                    // row.append("<p>Event name: " + data[i].city + ", " + data[i].state + " " + data[i].zip_code + "<p>");
+                    // row.append("<p>Event name: " + data[i].state + "<p>");
+                    // row.append("<p>Event name: " + data[i].phone_number + "<p>");
+                    // // row.append("<p>Event name: " + data[i].time_slots + "<p>");
+                    // $("#event-box").prepend(row);
                 };
             };
         });
